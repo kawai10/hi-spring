25d7715349134fcc5be58ea2a316d3486546f66f
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-10 17:03:32 +0900

    프로젝트 생성

--- src/main/java/hi/hispring/HiSpringApplication.java
+++ src/main/java/hi/hispring/HiSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HiSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HiSpringApplication.class, args);
 	}
 
