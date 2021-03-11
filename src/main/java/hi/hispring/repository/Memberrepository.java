b6ec252abd6c19f6b470fc4dd99c137bceb10d0a
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-11 09:15:10 +0900

    Domain, Repository

---
+++ src/main/java/hi/hispring/repository/Memberrepository.java
@@ -0,0 +1,15 @@
+package hi.hispring.repository;
+
+import hi.hispring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Memberrepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+
+}
