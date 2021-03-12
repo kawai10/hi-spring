a706697d3a38439b87c9996448f6fc928583c93d
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-12 15:10:22 +0900

    회원 등록

--- src/main/java/hi/hispring/controller/MemberController.java
+++ src/main/java/hi/hispring/controller/MemberController.java
@@ -2,9 +2,12 @@ package hi.hispring.controller;
 
 
 
+import hi.hispring.domain.Member;
 import hi.hispring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -16,4 +19,21 @@ public class MemberController {
 
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+
 }
