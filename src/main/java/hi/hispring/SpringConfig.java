a87d0c0c3e7eb29200e4d87d6e7e25a647ea6fab
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-14 09:36:44 +0900

    Jdbc Template

--- src/main/java/hi/hispring/SpringConfig.java
+++ src/main/java/hi/hispring/SpringConfig.java
@@ -1,22 +1,29 @@
 package hi.hispring;
 
+import hi.hispring.repository.JdbcTemplateMemberRepository;
 import hi.hispring.repository.Memberrepository;
 import hi.hispring.repository.MemoryMemberRepository;
 import hi.hispring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource; }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public Memberrepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+// return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
