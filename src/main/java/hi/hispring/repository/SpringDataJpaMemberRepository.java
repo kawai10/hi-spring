0fabc6c5a4afeedd9c7d11fd2ac25aa0a41b5951
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-03-14 11:39:56 +0900

    Spring data JPA

---
+++ src/main/java/hi/hispring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,13 @@
+package hi.hispring.repository;
+
+
+import hi.hispring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
